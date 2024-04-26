157dbfd2af3fbadf363200d4f58fba1cc7aa03a4
NI Corp. <123ababab123ababab@gmail.com>
2024-04-27 01:34:11 +0300

    activity check ui 2

--- app/src/main/java/com/nicorp/paymentv/MainActivity.java
+++ app/src/main/java/com/nicorp/paymentv/MainActivity.java
@@ -55,7 +55,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        setContentView(R.layout.activity_main);
+        // Go to activity_check.xml
+         setContentView(R.layout.activity_check);
+
+//        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
