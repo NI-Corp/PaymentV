da031c915f939e19e87a881bfdbeefe25c0328c2
NI Corp. <123ababab123ababab@gmail.com>
2024-04-28 03:03:55 +0300

    Update

--- app/src/main/java/com/nicorp/paymentv/CheckActivity.java
+++ app/src/main/java/com/nicorp/paymentv/CheckActivity.java
@@ -1,13 +1,22 @@
 package com.nicorp.paymentv;
 
+import static android.app.PendingIntent.getActivity;
+
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.database.Observable;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -15,6 +24,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -22,9 +32,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.pengrad.telegrambot.model.request.Keyboard;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.function.Consumer;
 
+import co.kyash.rkd.KeyboardDetector;
+import co.kyash.rkd.KeyboardStatus;
 import raiffeisen.sbp.sdk.client.SbpClient;
 import raiffeisen.sbp.sdk.exception.ContractViolationException;
 import raiffeisen.sbp.sdk.exception.SbpException;
@@ -47,15 +62,50 @@ public class CheckActivity extends AppCompatActivity {
         return super.onKeyDown(keyCode, event);
     }
 
+
+    @SuppressLint("CheckResult")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check);
 
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
         codeEditText = findViewById(R.id.code_edit_text);
         checkStatusLayout = findViewById(R.id.check_status);
         checkStatusText = findViewById(R.id.check_status_text);
         checkButton = findViewById(R.id.check_button);
+        ConstraintLayout constraintLayout2 = findViewById(R.id.constraintLayout2);
+
+        codeEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    // Обработка события нажатия кнопки "OK" на клавиатуре
+                    // Вернуть true, чтобы предотвратить закрытие клавиатуры
+                    checkCode();
+                    return true;
+                }
+                return false; // Вернуть false, чтобы позволить стандартную обработку
+            }
+        });
+
+
+        KeyboardDetector keyboardDetector = new KeyboardDetector(this);
+        io.reactivex.rxjava3.core.Observable<KeyboardStatus> keyboardStatusObservable = keyboardDetector.observe();
+        keyboardStatusObservable.subscribe(keyboardStatus -> {
+            if (keyboardStatus == KeyboardStatus.CLOSED) {
+                // Keyboard is closed
+                ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) constraintLayout2.getLayoutParams();
+                layoutParams.topMargin = -100; // Adjust as needed
+                constraintLayout2.setLayoutParams(layoutParams);
+            } else if (keyboardStatus == KeyboardStatus.OPENED) {
+                // Keyboard is opened
+                ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) constraintLayout2.getLayoutParams();
+                layoutParams.topMargin = -800; // Adjust as needed
+                constraintLayout2.setLayoutParams(layoutParams);
+            }
+        });
 
         ImageView qrButton = findViewById(R.id.qr_button);
         qrButton.setOnClickListener(view -> {
@@ -75,6 +125,7 @@ public class CheckActivity extends AppCompatActivity {
             }
         });
     }
+
 
     private static final int SCAN_QR_REQUEST_CODE = 100;
 
